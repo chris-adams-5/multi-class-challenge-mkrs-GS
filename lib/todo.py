3c250ce4fd12b3f92479eb125221dcfd4083bffb
Chris Adams <cn10cra@gmailcom>
2026-04-22 11:38:53 +0100

    Todo class

---
+++ lib/todo.py
@@ -0,0 +1,24 @@
+
+class Todo:
+    # Public Properties:
+    #   task: a string representing the task to be done
+    #   complete: a boolean representing whether the task is complete
+
+    def __init__(self, task):
+        # Parameters:
+        #   task: a string representing the task to be done
+        # Side-effects:
+        #   Sets the task property
+        #   Sets the complete property to False
+        if not isinstance(task, str):
+            raise Exception("Error Todo takes a string")
+        
+        self.task = task
+        self.complete = False
+
+    def mark_complete(self):
+        # Returns:
+        #   Nothing
+        # Side-effects:
+        #   Sets the complete property to True
+        self.complete = True
